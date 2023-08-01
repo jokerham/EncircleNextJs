5c3c3d907bde90cbea3d48b749dc6a0fb14621f6
unknown <3424@mizuho-int.com>
2023-08-01 11:21:17 +0900

    Update middleware.tsx
    Error in request url modified.

--- middleware.tsx
+++ middleware.tsx
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  if (url.pathname === '/') {
+  if (request.url.pathname === '/') {
     return NextResponse.rewrite(new URL('/blogpage/home', request.url))
   }
 }
